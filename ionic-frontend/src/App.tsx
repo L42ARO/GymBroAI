2557a3ee07e22712102a7ad11f6d71519a0b2b9c
L42ARO <alvaro512@usf.edu>
2023-10-21 17:36:19 -0400

    changed names and icons

--- ionic-frontend/src/App.tsx
+++ ionic-frontend/src/App.tsx
@@ -10,10 +10,10 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Home';
-import Tab2 from './pages/Chat';
-import Tab3 from './pages/Settings';
+import { chatbubbleEllipses, ellipse, home, person, square, text, triangle } from 'ionicons/icons';
+import Home from './pages/Home';
+import Chat from './pages/Chat';
+import Settings from './pages/Settings';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -42,31 +42,28 @@ const App: React.FC = () => (
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
+          <Route exact path="/home">
+            <Home />
           </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
+          <Route exact path="/chat">
+            <Chat />
           </Route>
-          <Route path="/tab3">
-            <Tab3 />
+          <Route path="/settings">
+            <Settings />
           </Route>
           <Route exact path="/">
-            <Redirect to="/tab1" />
+            <Redirect to="/home" />
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon aria-hidden="true" icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+          <IonTabButton tab="home" href="/home">
+            <IonIcon aria-hidden="true" icon={home} />
           </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon aria-hidden="true" icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+          <IonTabButton tab="chat" href="/chat">
+            <IonIcon aria-hidden="true" icon={chatbubbleEllipses} />
           </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon aria-hidden="true" icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+          <IonTabButton tab="settings" href="/settings">
+            <IonIcon aria-hidden="true" icon={person} />
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
